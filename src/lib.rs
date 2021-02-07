3958a39937445c87006fe64add446ef7e5274313
Anders429 <anders.stefan.evensen@gmail.com>
2021-02-06 17:48:39 -0800

    Implement RangeBounds for RangeFromExclusive<&T>.

--- src/lib.rs
+++ src/lib.rs
@@ -35,6 +35,15 @@ impl<T> RangeBounds<T> for RangeFromExclusive<T> {
     }
 }
 
+impl<T> RangeBounds<T> for RangeFromExclusive<&T> {
+    fn start_bound(&self) -> Bound<&T> {
+        Excluded(self.start)
+    }
+    fn end_bound(&self) -> Bound<&T> {
+        Unbounded
+    }
+}
+
 /// A range bounded exclusively below and inclusively above.
 ///
 /// The `RangeFromExclusiveToInclusive` contains all values with `x > start` and `x <= end`. It is
@@ -50,6 +59,15 @@ impl<T> RangeBounds<T> for RangeFromExclusiveToInclusive<T> {
     }
     fn end_bound(&self) -> Bound<&T> {
         Included(&self.end)
+    }
+}
+
+impl<T> RangeBounds<T> for RangeFromExclusiveToInclusive<&T> {
+    fn start_bound(&self) -> Bound<&T> {
+        Excluded(self.start)
+    }
+    fn end_bound(&self) -> Bound<&T> {
+        Included(self.end)
     }
 }
 
@@ -71,13 +89,22 @@ impl<T> RangeBounds<T> for RangeFromExclusiveToExclusive<T> {
     }
 }
 
+impl<T> RangeBounds<T> for RangeFromExclusiveToExclusive<&T> {
+    fn start_bound(&self) -> Bound<&T> {
+        Excluded(self.start)
+    }
+    fn end_bound(&self) -> Bound<&T> {
+        Excluded(self.end)
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use ::{RangeFromExclusive, RangeFromExclusiveToExclusive, RangeFromExclusiveToInclusive};
     use core::ops::{
         Bound::{Excluded, Included, Unbounded},
         RangeBounds,
     };
+    use {RangeFromExclusive, RangeFromExclusiveToExclusive, RangeFromExclusiveToInclusive};
 
     #[test]
     fn range_from_exclusive_range_bounds() {
@@ -85,6 +112,14 @@ mod tests {
 
         assert_matches!(range.start_bound(), Excluded(1));
         assert_matches!(range.end_bound(), Unbounded);
+    }
+
+    #[test]
+    fn range_from_exclusive_range_bounds_borrowed() {
+        let range = RangeFromExclusive { start: &1 };
+
+        assert_matches!(RangeBounds::<usize>::start_bound(&range), Excluded(1));
+        assert_matches!(RangeBounds::<usize>::end_bound(&range), Unbounded);
     }
 
     #[test]
@@ -96,10 +131,26 @@ mod tests {
     }
 
     #[test]
+    fn range_from_exclusive_to_exclusive_range_bounds_borrowed() {
+        let range = RangeFromExclusiveToExclusive { start: &1, end: &3 };
+
+        assert_matches!(RangeBounds::<usize>::start_bound(&range), Excluded(1));
+        assert_matches!(RangeBounds::<usize>::end_bound(&range), Excluded(3));
+    }
+
+    #[test]
     fn range_from_exclusive_to_inclusive_range_bounds() {
         let range = RangeFromExclusiveToInclusive { start: 1, end: 3 };
 
         assert_matches!(range.start_bound(), Excluded(1));
         assert_matches!(range.end_bound(), Included(3));
+    }
+
+    #[test]
+    fn range_from_exclusive_to_inclusive_range_bounds_borrowed() {
+        let range = RangeFromExclusiveToInclusive { start: &1, end: &3 };
+
+        assert_matches!(RangeBounds::<usize>::start_bound(&range), Excluded(1));
+        assert_matches!(RangeBounds::<usize>::end_bound(&range), Included(3));
     }
 }
